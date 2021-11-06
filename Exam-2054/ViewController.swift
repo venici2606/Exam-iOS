59bc6d85fd0b7107f344585c7820d896f97187b3
venici2606 <venicia_p@live.no>
2021-11-06 10:43:32 +0100

    Initial Commit

---
+++ Exam-2054/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exam-2054
+//
+//  Created by Venicia Punithan on 06/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
