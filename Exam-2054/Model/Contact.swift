d182b00be2892d98d3dd6a4f1391a8e0ca619d1b
venici2606 <venicia_p@live.no>
2021-11-22 18:48:38 +0100

    Added model folder and contact file again

---
+++ Exam-2054/Model/Contact.swift
@@ -0,0 +1,52 @@
+//
+//  Contact.swift
+//  Exam-2054
+//
+//  Created by Venicia Punithan on 06/11/2021.
+//
+
+import Foundation
+
+struct ContactsData: Codable {
+    let results: [Contact]
+}
+
+struct Contact: Codable {
+    //let id: ID
+    var name: Name
+    var picture: Picture
+    var dob: Dob
+    let email: String
+    let phone: String
+    var location: Location
+}
+
+struct ID: Codable {
+    var value: String
+}
+
+struct Name: Codable {
+    var first: String
+    var last: String
+}
+
+struct Picture: Codable {
+    var large: URL
+    var medium: URL
+    var thumbnail: URL
+}
+
+struct Location: Codable {
+    var city: String
+    var coordinates: Coordinates?
+}
+
+struct Coordinates: Codable {
+    var latitude: String?
+    var longitude: String?
+}
+
+struct Dob: Codable {
+    var date: String
+    var age: Int
+}
